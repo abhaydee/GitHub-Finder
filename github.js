c9c2b7f56bf49eeccd18c6d42d98765d7a7190e0
abhaydee <abhaygirish1997@gmail.com>
2018-10-27 00:59:02 -0700

    changes to github.js

--- github.js
+++ github.js
@@ -8,7 +8,7 @@ class github {
     async getuser(user) {
         const profileresponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const profile = await profileresponse.json();
-        const reporesponse = await fetch(`https:/api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id} &client+secret=${this.client_secret}}`);
+        const reporesponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id} &client+secret=${this.client_secret}}`);
         const repos = await reporesponse.json();
         return {
             profile,
